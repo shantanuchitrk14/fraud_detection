da6661133492bbb329970315eacd53247c7a6919
shantanuchitrak <shantanuchitrk14@gmail.com>
2021-11-29 13:41:36 +0530

    added new 3 model ML code

---
+++ ml_model.py
@@ -0,0 +1,11 @@
+
+print('adding all ML realted code here')
+
+def model1():
+    print('adding code for ML model1')
+
+def model2():
+    print('adding code for ML model2')
+
+def model3():
+    print('adding code for ML model3')
